77f7fbece049d290b2555a51b738fabffaa757e7
Martín Farias <martin.akg1991@gmail.com>
2025-12-08 01:28:17 -0300

    changes favicon. redesign portfolio. new media

--- views/Portfolio.js
+++ views/Portfolio.js
@@ -1,26 +1,20 @@
 export default {
   template: /*html*/`
     <div class="portfolio container">
-      <div>
-        <div class="content">
-          <h2>Kimono</h2>
-        </div>
-      </div>
-      <div>
-        <div class="content">
-          <h2>Arcade</h2>
-        </div>
-      </div>
-      <div>
-        <div class="content">
-          <h2>Bass</h2>
-        </div>
-      </div>
-      <div>
-        <div class="content">
-          <h2>Chill</h2>
-        </div>
+      <div v-for="i in 28" :key="i" @click="onSelect">
       </div>
     </div>
   `,
+  methods: {
+    onSelect(event) {
+      if (event.target.classList.contains('zoom')) {
+        event.target.classList.remove('zoom');
+      } else {
+        for (const el of event.target.parentNode.children) {
+          el.classList.remove('zoom');
+        }
+        event.target.classList.add('zoom');
+      }
+    }
+  }
 };
